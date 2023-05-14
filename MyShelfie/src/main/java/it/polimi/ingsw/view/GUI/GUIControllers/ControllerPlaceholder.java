46bb30969bc92e7cba0a0eb7bf35e1a188699a8d
pmmarzola <piermatteo.marzola@mail.polimi.it>
2023-05-14 11:55:33 +0200

    Added GUI repository

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/ControllerPlaceholder.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.view.GUI.GUIControllers;
+
+//THIS IS A PLACEHOLDER DONT USE THS CLASS
+
+public class ControllerPlaceholder {
+}
---
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/GUI/GUIPlaceholder.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.view.GUI;
+
+//THIS IS A PLACEHOLDER DONT USE THS CLASS
+
+public class GUIPlaceholder {
+}
